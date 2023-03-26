e2b57ac65d7903c62171951335773ea4b0d0ab91
Eclipse <96087997+EclipseTheDevAltAcc@users.noreply.github.com>
2023-03-26 00:16:10 -0400

    Create discord-bot-template.js

---
+++ discord-bot-template.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js")
+const client = require("discord.js")
+const token = "SUPA_SECRET_TOKEN"
+
+client.on("ready") => {
+  console.log("Ready!!")
+}
+
+client.on("message") => {
+  
+}
+
+client.login(token)
